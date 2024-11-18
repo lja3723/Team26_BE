b15ac48ff7442c0e279c786802e51a8983f37a58
lja3723 <lja3723@gmail.com>
2024-11-18 23:57:05 +0900

    refactor: http 메서드수정

--- src/main/java/org/ktc2/cokaen/wouldyouin/reservation/api/KakaoPaymentRedirectController.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/reservation/api/KakaoPaymentRedirectController.java
@@ -3,7 +3,7 @@ package org.ktc2.cokaen.wouldyouin.reservation.api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -19,17 +19,17 @@ public class KakaoPaymentRedirectController {
     @Value("${oauth.payment.fail_deep_link}")
     private String failDeepLink;
 
-    @PostMapping("/kakaopay/redirect/approval")
+    @GetMapping("/kakaopay/redirect/approval")
     public String redirectKakaopayApproval(@RequestParam("pg_token") String pgToken) {
         return "redirect:" + approvalDeepLink + "?pg_token=" + pgToken;
     }
 
-    @PostMapping("/kakaopay/redirect/cancel")
+    @GetMapping("/kakaopay/redirect/cancel")
     public String redirectKakaopayCancel(@RequestParam("pg_token") String pgToken) {
         return "redirect:" + cancelDeepLink + "?pg_token=" + pgToken;
     }
 
-    @PostMapping("/kakaopay/redirect/fail")
+    @GetMapping("/kakaopay/redirect/fail")
     public String redirectKakaopayFail(@RequestParam("pg_token") String pgToken) {
         return "redirect:" + failDeepLink + "?pg_token=" + pgToken;
     }

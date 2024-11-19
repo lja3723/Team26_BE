e8e36660b4d13d59aafcac30e1375a418c844fc1
daolove0323 <daolove0323@gmail.com>
2024-11-20 03:40:08 +0900

    Update PaymentService.java

--- src/main/java/org/ktc2/cokaen/wouldyouin/payment/application/PaymentService.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/payment/application/PaymentService.java
@@ -70,7 +70,7 @@ public class PaymentService {
         payment.setTid(kakaoPayResponse.getTid());
         paymentRepository.save(payment);
         paymentRepository.flush();
-        return kakaoPayResponse.getNextRedirectPcUrl() + "?orderId=" + payment.getPartnerOrderId();
+        return kakaoPayResponse.getNextRedirectAppUrl() + "?orderId=" + payment.getPartnerOrderId();
     }
 
     @Transactional

a04be923b3ea143b9b7eb946f439e78b01c573e0
daolove0323 <daolove0323@gmail.com>
2024-11-23 05:07:19 +0900

    Update KakaoPaymentRedirectController.java

--- src/main/java/org/ktc2/cokaen/wouldyouin/reservation/api/KakaoPaymentRedirectController.java
+++ src/main/java/org/ktc2/cokaen/wouldyouin/reservation/api/KakaoPaymentRedirectController.java
@@ -2,16 +2,11 @@ package org.ktc2.cokaen.wouldyouin.reservation.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ktc2.cokaen.wouldyouin._common.api.ApiResponse;
-import org.ktc2.cokaen.wouldyouin._common.api.ApiResponseBody;
 import org.ktc2.cokaen.wouldyouin.payment.application.PaymentService;
-import org.ktc2.cokaen.wouldyouin.reservation.api.dto.KakaoPayReservationResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -35,15 +30,16 @@ public class KakaoPaymentRedirectController {
     }
 
     @GetMapping("/kakaopay/redirect/approval")
-    public String redirectKakaopayApproval(
+    public void redirectKakaopayApproval(
         @RequestParam("pg_token") String pgToken, @RequestParam(defaultValue = "1") Long orderId) {
         log.debug("실행되는지 확인 테스트 pg: {}, id: {}", pgToken, orderId);
 //        paymentService.approvePaymentTest(orderId, pgToken);
 //        return "redirect:/payview";
 //        return "redirect:" + "wouldyouin://booking/kakao/check/payment_approve" + "?reservationId=" + "1";
 //            paymentService.approvePayment(orderId, pgToken);
-        Long reservationId = paymentService.approvePayment(orderId, pgToken);
-        return "redirect:" + "wouldyouin://booking/kakao/check/payment_approve" + "?reservationId=" + reservationId;
+//        Long reservationId =
+        paymentService.approvePaymentTest(orderId, pgToken);
+//        return "redirect:" + "wouldyouin://booking/kakao/check/payment_approve" + "?reservationId=" + reservationId;
     }
 
     @GetMapping("payview")
